97bf226507ab0f20dd71e4fa12e68a42f28536de
Mushus <mushus.wynd@gmail.com>
2017-06-03 15:12:36 +0900

    プロジェクト追加

---
+++ MyGameStatus/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MyGameStatus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.status1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "MyGameStatus";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(109, 54);
+            this.contextMenuStrip.Opened += new System.EventHandler(this.contextMenuStrip_Opened);
+            // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status1ToolStripMenuItem});
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.statusToolStripMenuItem.Text = "Status";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(105, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.exitToolStripMenuItem.Text = "Exit(&X)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // status1ToolStripMenuItem
+            // 
+            this.status1ToolStripMenuItem.Name = "status1ToolStripMenuItem";
+            this.status1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.status1ToolStripMenuItem.Text = "status1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem status1ToolStripMenuItem;
+    }
+}
+
---
+++ MyGameStatus/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGameStatus
+{
+    public partial class Form1 : Form
+    {
+        private Process hProcess = null;
+        private string processName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 自身のプロセスを取得する
+            hProcess = Process.GetCurrentProcess();
+            processName = hProcess.ProcessName;
+            Visible = false;
+            // プロセス名を設定
+            statusToolStripMenuItem.Text = processName;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void contextMenuStrip_Opened(object sender, EventArgs e)
+        {
+            // 一覧を作る
+            string dir = System.Environment.CurrentDirectory;
+            string[] files = Directory.GetFiles(dir);
+
+            ToolStripDropDownMenu statuses = new ToolStripDropDownMenu();
+
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) != ".exe")
+                {
+                    continue;
+                }
+
+                string name = Path.GetFileNameWithoutExtension(file);
+                bool isMyProcess = (processName == name);
+
+                // items
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Text = name;
+                item.Checked = isMyProcess;
+                if (!isMyProcess)
+                {
+                    item.Click += new System.EventHandler(selectStatus);
+                }
+
+                statuses.Items.Add(item);
+            }
+
+            statusToolStripMenuItem.DropDown = statuses;
+        }
+
+        private void selectStatus(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+            string fileName = item.Text;
+
+            string dir = System.Environment.CurrentDirectory;
+            string exePath = dir + @"\" + fileName + ".exe";
+
+            Process p = Process.Start(exePath);
+
+            this.Close();
+        }
+    }
+}
